1d9f220ccf99b3fd3abf7020997ef36a7856a4a7
Bernhard Pilsl <pilsl.bernhard@gmail.com>
2022-12-03 17:48:34 +0100

    also parsing parameters without default values

--- utils/module_parser.py
+++ utils/module_parser.py
@@ -12,11 +12,18 @@ def parse_module(file_name):
     params = {}
     f = open(file_name, 'r')
     for line in f:
-        match = re.search(r'config_.setDefault.+"(.+)"\w*,\w*(.+(?=\)))', line)
-        if match:
-            key = match.group(1)
-            default_val = match.group(2)
+        match_default = re.search(r'config_.setDefault.+"(.+)"\w*,\w*(.+(?=\)))', line)
+        if match_default:
+            key = match_default.group(1)
+            default_val = match_default.group(2)
             params[key] = default_val
+            # there are config keys without default values
+            # we also need to capture those
+            # defaulted parameters have priority though
+        match_no_default = re.search(r'config_\.get.+\("(.+?)"', line)
+        if match_no_default:
+            if not params.keys().__contains__(match_no_default.group(1)):
+                params[match_no_default.group(1)] = ''  # no default value
 
     retval['parameters'] = params
     return retval
@@ -25,29 +32,21 @@ def parse_module(file_name):
 directory = sys.argv[1]
 output_file = sys.argv[2]
 out = open(output_file, 'w')
-out.write('[')
-modules = {}
+modules = []
 globalConfig = {"module_name": "Corryvreckan", "parameters": {"log_level": "INFO", "log_format": "",
-                                                          "purge_output_directory": "false",
-                                                          "detectors_file": "detectors.geo",
-                                                          "detectors_file_updated": "detectors_updated.geo",
-                                                          "histogram_file": "out.root",
-                                                          "number_of_tracks": "-1",
-                                                          "number_of_events": "-1"
-                                                          }}
-out.write(json.dumps(globalConfig))
-out.write(',\n\n')
-first = True
+                                                              "purge_output_directory": "false",
+                                                              "detectors_file": "detectors.geo",
+                                                              "detectors_file_updated": "detectors_updated.geo",
+                                                              "histogram_file": "out.root",
+                                                              "number_of_tracks": "-1",
+                                                              "number_of_events": "-1"
+                                                              }}
+modules.append(globalConfig)
 files = glob.glob(os.path.join(directory, '**/*.cpp'))
 for filename in files:
-    if first:
-        first = False
-    else:
-        out.write(' ,\n\n')
     if os.path.isfile(filename):
         print('processing ', filename)
         module = parse_module(filename)
-        description = json.dumps(module)
-        out.write(description)
+        modules.append(module)
 
-out.write(']')
+out.write(json.dumps(modules, indent=4))
